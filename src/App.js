eb167e567d246894cc66efca63a0c283d3b76080
Enrique Narciso <46304338+e-narciso@users.noreply.github.com>
2019-10-04 16:21:15 -0400

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import Listing from "./components/Listing";
 import GoogleMaps from "./components/Maps";
 import berlin from "./images/berlin.jpg";
 import Video from "./components/Video";
-import logo from "./images/airbnb-2.svg"
-import Navbar from './components/Navbar'
-
+import logo from "./images/airbnb-2.svg";
+import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="main">
-    <Video />
-    <Navbar />
+      <Video />
+      <Navbar />
       <div className="papa-div container">
-        
         <div className="row">
           <div className="col-12 search-div">
-          <img id="logo" src={logo} alt=""></img>
-          <h3 className="city-header">BERLIN</h3>
-          <input id="search-bar" type="text" placeholder="Search Berlin..."></input>
+            <img id="logo" src={logo} alt=""></img>
+            <h3 className="city-header">BERLIN</h3>
+            <input
+              id="search-bar"
+              type="text"
+              placeholder="Search Berlin..."
+            ></input>
           </div>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-    render () {
-        return (
-            <nav className="navbar navbar-expand-lg">
-
-            <div className="navlinks">
-              <a id="help-button" className="navbar-brand" href="#">
-                Help
-              </a>
-              <a className="navbar-brand" href="#">
-                Sign Up
-              </a>
-              <a className="navbar-brand" href="#">
-                Login
-              </a>
-            </div>
-          </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg">
+        <div className="navlinks">
+          <a id="help-button" className="navbar-brand" href="#">
+            Help
+          </a>
+          <a className="navbar-brand" href="#">
+            Sign Up
+          </a>
+          <a className="navbar-brand" href="#">
+            Login
+          </a>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
