8bfc198a9239d6a282ad40a708d03411e7b20beb
alejandrogomez <alegomez1@me.com>
2019-10-04 15:06:42 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,27 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Listing from "./components/Listing";
 import GoogleMaps from "./components/Maps"
-import GoogleMapReact from 'google-map-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-        <Listing />
-        <h1>MAPS DIV</h1>
-        <GoogleMaps/>
-      </header>
+        
     </div>
   );
 }
