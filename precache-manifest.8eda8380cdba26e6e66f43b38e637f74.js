c40aa34d3f5b5f888c4f16569981a36349dc0d73
Enrique Narciso <46304338+e-narciso@users.noreply.github.com>
2019-10-04 09:46:15 -0400

    Updates

---
+++ precache-manifest.8eda8380cdba26e6e66f43b38e637f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09d5549482d9def2ec4aa290f180236",
+    "url": "/Hackathon-React/index.html"
+  },
+  {
+    "revision": "ad5a8c73274403b8e1cc",
+    "url": "/Hackathon-React/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "23befe4542cadfd5747a",
+    "url": "/Hackathon-React/static/js/2.ba1a6784.chunk.js"
+  },
+  {
+    "revision": "ad5a8c73274403b8e1cc",
+    "url": "/Hackathon-React/static/js/main.9b68986e.chunk.js"
+  },
+  {
+    "revision": "e0a30f39f3db609cd9a3",
+    "url": "/Hackathon-React/static/js/runtime-main.6b9d7cd1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Hackathon-React/static/media/logo.25bf045c.svg"
+  }
+]);
