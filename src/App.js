bef5b8158650bee88615ee1bd356039507784f26
alejandrogomez <alegomez1@me.com>
2019-10-04 16:09:07 -0400

    Homepage done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Listing from "./components/Listing";
 import GoogleMaps from "./components/Maps";
 import berlin from "./images/berlin.jpg";
 import Video from "./components/Video";
+import logo from "./images/airbnb-2.svg"
 
 function App() {
   return (
     <div className="main">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          AriBnB
-        </a>
+    <Video />
+      <nav className="navbar navbar-expand-lg">
 
-        <div>
+        <div className="navlinks">
+          <a id="help-button" className="navbar-brand" href="#">
+            Help
+          </a>
           <a className="navbar-brand" href="#">
-            SignUp
+            Sign Up
           </a>
           <a className="navbar-brand" href="#">
             Login
           </a>
-          <a className="navbar-brand" href="#">
-            Search
-          </a>
         </div>
       </nav>
       <div className="papa-div container">
-        <Video />
+        
         <div className="row">
           <div className="col-12 search-div">
+          <img id="logo" src={logo} alt=""></img>
           <h3 className="city-header">BERLIN</h3>
           <input id="search-bar" type="text" placeholder="Search Berlin..."></input>
           </div>
