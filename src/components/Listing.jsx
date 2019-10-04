5e2764e4606537553a172e36fdaf3cd56a4732ae
Enrique Narciso <46304338+e-narciso@users.noreply.github.com>
2019-10-04 10:00:03 -0400

    Listing component started

---
+++ src/components/Listing.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Listing extends Component {
+    render() {
+        return (
+            <div>
+                some kinda listing info I guess
+            </div>
+        );
+    }
+}
+
+export default Listing;
